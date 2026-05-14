2033bf78c556ae3a4d231a6f9156fe62c7d460e3
daildubath <daildubath@byui.edu>
2026-05-14 14:42:46 -0600

    removed ignored files

--- Connect4Rust/target/debug/Connect4.d
+++
@@ -1,1 +0,0 @@
-/home/benge/Code/CSE310/CSE310Project1/TestPrograms/Connect4/target/debug/Connect4: /home/benge/Code/CSE310/CSE310Project1/TestPrograms/Connect4/src/main.rs
--- Connect4Rust/target/debug/deps/Connect4-c894caa3299009be.d
+++
@@ -1,5 +0,0 @@
-/home/benge/Code/CSE310/CSE310Project1/TestPrograms/Connect4/target/debug/deps/libConnect4-c894caa3299009be.rmeta: src/main.rs
-
-/home/benge/Code/CSE310/CSE310Project1/TestPrograms/Connect4/target/debug/deps/Connect4-c894caa3299009be.d: src/main.rs
-
-src/main.rs:
